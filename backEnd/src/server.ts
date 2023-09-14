eb39dca41abd679d1d0172f1bb13b21437b709b6
Rayaninha <salesrayane11@gmail.com>
2023-09-14 14:16:46 -0300

    :zap: perf: add environmental variables

--- backEnd/src/server.ts
+++ backEnd/src/server.ts
@@ -1,9 +1,10 @@
 import { app } from "./app";
+import { env } from "./env";
 
 app
   .listen({
     host: "0.0.0.0",
-    port: 8080,
+    port: env.PORT,
   })
   .then(() => {
     console.log("🚀 HTTP Server Running!");
