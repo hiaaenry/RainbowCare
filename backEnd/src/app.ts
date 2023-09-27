fffe136ccc0a5ebf75407853589a61a17529d300
Rayaninha <salesrayane11@gmail.com>
2023-09-27 12:08:12 -0300

    :sparkles: feat: creation of a simple user

--- backEnd/src/app.ts
+++ backEnd/src/app.ts
@@ -1,3 +1,23 @@
 import fastity from "fastify";
+import { z } from "zod";
+import { prisma } from "./lib/prisma";
 
 export const app = fastity();
+
+app.post("/users", async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+  });
+
+  const { name, email } = registerBodySchema.parse(request.body);
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  });
+
+  return reply.status(201).send();
+});
