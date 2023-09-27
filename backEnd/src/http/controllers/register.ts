705ac18227120d3fab1121294ef9a47d130d305f
Rayaninha <salesrayane11@gmail.com>
2023-09-27 12:55:16 -0300

    :boom: fix: error status code

--- backEnd/src/http/controllers/register.ts
+++ backEnd/src/http/controllers/register.ts
@@ -18,7 +18,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       password,
     });
   } catch (error) {
-    return reply.status(404).send();
+    return reply.status(409).send();
   }
 
   return reply.status(201).send();
