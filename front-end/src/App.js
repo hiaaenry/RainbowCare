12f15af5e9653db113a3228383d9d6352a58cb64
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2023-10-04 21:43:56 -0300

    Imagens

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/pages/Navbar';
+import Home from './components/pages/Home';
+import Sobre from './components/pages/Sobre';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Navbar />
+       <Home />
+       <Sobre />
     </div>
-  );
+  )
 }
 
 export default App;
