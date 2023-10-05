3b19b3ab4047bf8e2d1050f0992fd0f9e6a8e67d
Rayaninha <salesrayane11@gmail.com>
2023-10-05 08:57:02 -0300

    :test_tube: test: make_user for controller tests

---
+++ backEnd/src/utils/test/create_and_authenticate_user.ts
@@ -0,0 +1,21 @@
+import request from "supertest";
+import { FastifyInstance } from "fastify";
+
+export async function createAndAuthenticateUser(app: FastifyInstance) {
+  await request(app.server).post("/users").send({
+    name: "Test Name",
+    email: "test.email@example.com",
+    password: "test.password",
+  });
+
+  const authResponse = await request(app.server).post("/sessions").send({
+    email: "test.email@example.com",
+    password: "test.password",
+  });
+
+  const { token } = authResponse.body;
+
+  return {
+    token,
+  };
+}
