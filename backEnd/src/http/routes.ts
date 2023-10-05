946721bba99f0b2a7685e30fedf4509ac5b7af49
Desiree Fernandes <72208750+Rayaninha@users.noreply.github.com>
2023-10-04 21:17:01 -0300

    :sparkles: feat: notification controller

--- backEnd/src/http/routes.ts
+++ backEnd/src/http/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register";
 import { authenticate } from "./controllers/authenticate";
+import { sendNotification } from "./controllers/notification";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
+  app.post("/send-notification", sendNotification);
 }
