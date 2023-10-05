2670790277fa1ce429eab6e1322416f8ad3c2ad1
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2023-10-04 21:40:00 -0300

    #5 - Navbar, Início e Sobre

---
+++ front-end/src/components/pages/Home.js
@@ -0,0 +1,27 @@
+
+
+function Home(){
+    return(
+        <div className='home'>
+            <div className='center'>
+                <div className='home-page'>
+                    <div className='img-home1'>
+                        <div className='page1-2'>
+                            <img  src="page1-2.png"/>
+                        </div>
+                        <div className='page1-1'>
+                            <img src="page1-1.png"/>       
+                            <p className="pa">A Plataforma de Acolhimento LGBT+ foi criada com o compromisso inabalável de oferecer um espaço seguro, inclusivo e acolhedor para todas as pessoas que fazem parte da comunidade LGBT+ e seus aliados. Nossa missão é fornecer suporte, educação e recursos para promover a igualdade, a diversidade e a acessibilidade.
+                             <br /> <br /><button>Saiba mais</button>
+                            </p>
+                                          
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
---
+++ front-end/src/components/pages/Navbar.js
@@ -0,0 +1,33 @@
+function Navbar(){
+  return(
+      <div className='navbar'>
+        <div className='center'>
+          <div className='menu'>
+            
+            <div className='logo'>
+              <img src="cat.png" alt="Cat" />
+            </div>
+
+            <div className='logo-title'>
+              <h3>RainbowCare</h3>
+            </div>
+
+            <div className='item-menu'>
+              <a  href='#'>Início</a>
+              <a href='#'>Sobre</a>
+              <a href='#'>Depoimentos</a>
+              <a href='#'>Contatos</a>
+            </div>
+
+            <div className='item-menu-but'>
+            <a href='#'>Login</a>
+            <a href='#'>Cadastro</a>
+            </div>
+          
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default Navbar;
