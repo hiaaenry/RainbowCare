2233c7dce83a9140682dae2f34974296d8b5cdbe
Rayaninha <salesrayane11@gmail.com>
2023-10-17 14:32:41 -0300

    :books: docs: users register route

--- backEnd/src/http/routes.ts
+++ backEnd/src/http/routes.ts
@@ -2,9 +2,10 @@ import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register";
 import { authenticate } from "./controllers/authenticate";
 import { sendNotification } from "./controllers/notification";
+import { swaggerRegisterRoute } from "@/swagger";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post("/users", register);
+  app.post("/users", swaggerRegisterRoute, register);
   app.post("/sessions", authenticate);
   app.post("/send-notification", sendNotification);
 }
--- backEnd/src/swagger.ts
+++ backEnd/src/swagger.ts
@@ -18,3 +18,54 @@ export const swaggerUiOptions = {
   routePrefix: "/docs",
   exposeRoute: true,
 };
+
+export const swaggerRegisterRoute = {
+  schema: {
+    description: "create user account",
+    tags: ["user"],
+    body: {
+      type: "object",
+      properties: {
+        name: { type: "string" },
+        email: { type: "string", format: "email" },
+        password: { type: "string", format: "password" },
+      },
+      examples: [
+        {
+          name: "user name",
+          email: "user.email@example.com",
+          password: "password",
+        },
+      ],
+    },
+    response: {
+      201: {
+        description: "CREATED",
+        type: "object",
+        properties: {
+          message: {
+            type: "object",
+          },
+        },
+      },
+      409: {
+        description: "CONFLICT",
+        type: "object",
+        properties: {
+          message: {
+            type: "object",
+          },
+        },
+      },
+      500: {
+        description: "INTERNAL SERVER ERROR",
+        type: "object",
+        properties: {
+          message: {
+            type: "object",
+          },
+        },
+      },
+    },
+  },
+};
