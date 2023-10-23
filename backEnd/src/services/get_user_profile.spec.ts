e40f67585db1910b93568b27e48eb32bf4003534
Rayaninha <salesrayane11@gmail.com>
2023-10-23 14:31:47 -0300

    :test_tube: test: get user profile

---
+++ backEnd/src/services/get_user_profile.spec.ts
@@ -0,0 +1,38 @@
+import { InMemoryUsersRepository } from "@/repositories/inMemory/in_memory_users_repository";
+import { beforeEach, describe, expect, it } from "vitest";
+import { GetUserProfileService } from "./get_user_profile";
+import { hash } from "bcryptjs";
+import { ResourceNotFoundError } from "./errors/resource_not_found_error";
+
+let usersRepository: InMemoryUsersRepository;
+let sut: GetUserProfileService;
+
+describe("Get User Profile Service", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    sut = new GetUserProfileService(usersRepository);
+  });
+
+  it("should be able to get user profile", async () => {
+    const createdUser = await usersRepository.create({
+      name: "User Test Name",
+      email: "email.test@example.com",
+      password_hash: await hash("passwordtest", 6),
+    });
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+    expect(user.name).toEqual("User Test Name");
+  });
+
+  it("should not be able to get user profile with wrong id", async () => {
+    expect(async () => {
+      await sut.execute({
+        userId: "non-existing-id",
+      });
+    }).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+});
