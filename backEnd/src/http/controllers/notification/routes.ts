2a64430d730a3c1cc1a1998ccd0bd83a8b71cc91
Rayaninha <salesrayane11@gmail.com>
2023-10-23 18:01:02 -0300

    :truck: chore: rename notification controller

--- backEnd/src/http/controllers/notification/routes.ts
+++ backEnd/src/http/controllers/notification/routes.ts
@@ -1,6 +1,6 @@
 import { middlewareSendNotificationRoute } from "@/http/middlewares/middleware";
 import { FastifyInstance } from "fastify";
-import { sendNotification } from "./notification";
+import { sendNotification } from "./send";
 
 export async function notificationRouter(app: FastifyInstance) {
   app.post(
