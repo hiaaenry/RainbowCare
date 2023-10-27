d56faec49b3da0650a29bd173e54be3de2ce3527
Rayaninha <salesrayane11@gmail.com>
2023-10-27 15:10:08 -0300

    :test_tube: test: authenticate controller

---
+++ backEnd/src/http/controllers/users/authenticate.spec.ts
@@ -0,0 +1,32 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it, test } from "vitest";
+
+describe("Authenticate Controller (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to authenticate", async () => {
+    await request(app.server).post("/users").send({
+      name: "Test Name",
+      email: "test.email@example.com",
+      password: "test.password",
+      role: "USER",
+    });
+
+    const response = await request(app.server).post("/sessions").send({
+      email: "test.email@example.com",
+      password: "test.password",
+    });
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    });
+  });
+});
