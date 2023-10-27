2ef462f6dec99a2f2bfdebbd651b7db7f0621114
Rayaninha <salesrayane11@gmail.com>
2023-10-27 16:37:58 -0300

    :test_tube: test: create foster home controller

---
+++ backEnd/src/http/controllers/fosterHome/create.spec.ts
@@ -0,0 +1,29 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it, test } from "vitest";
+import { createAndAuthenticateUser } from "@/utils/test/create_and_authenticate_user";
+
+describe("Create Foster Home Controller (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to create foster home", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    const response = await request(app.server)
+      .post("/foster-home")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        name: "Test Foster Home",
+      });
+
+    expect(response.statusCode).toBe(201);
+
+    expect(response.statusCode).toBe(201);
+  });
+});
