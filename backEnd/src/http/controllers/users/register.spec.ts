61357cf5090e498f2e3230878415b81c636409bf
Rayaninha <salesrayane11@gmail.com>
2023-10-27 14:27:18 -0300

    :test_tube: test: user register controller

---
+++ backEnd/src/http/controllers/users/register.spec.ts
@@ -0,0 +1,24 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it, test } from "vitest";
+
+describe("Register Controller (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to register", async () => {
+    const response = await request(app.server).post("/users").send({
+      name: "Test Name",
+      email: "test.email@example.com",
+      password: "test.password",
+      role: "USER",
+    });
+
+    expect(response.statusCode).toBe(201);
+  });
+});
