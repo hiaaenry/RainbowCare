184a11336e346b67ad0f0413b747499e5033e2e9
desireefernandes <desireefernandes00@gmail.com>
2023-11-07 23:55:21 -0300

    :dizzy: feat: creating initial structure to login page

---
+++ front-end/src/Login.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function Login() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default Login;
