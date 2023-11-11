221d9a3956c2887dfb839310a4cb8aad711f48fa
desireefernandes <desireefernandes00@gmail.com>
2023-11-11 00:20:20 -0300

    :dizzy: feat: creating initial structure to home
    Co-authored-by: Gleicianegaldino <gleicianegaldino25@gmail.com>

--- front-end/src/components/pages/Home.jsx
+++ front-end/src/components/pages/Home.jsx
@@ -1,26 +1,23 @@
-
-
-function Home(){
-    return(
-        <div className='home'>
-            <div className='center'>
-                <div className='home-page'>
-                    <div className='img-home1'>
-                        <div className='page1-2'>
-                            <img  src="page1-2.png"/>
-                        </div>
-                        <div className='page1-1'>
-                            <img src="page1-1.png"/>       
-                            <p className="pa">A Plataforma de Acolhimento LGBT+ foi criada com o compromisso inabalável de oferecer um espaço seguro, inclusivo e acolhedor para todas as pessoas que fazem parte da comunidade LGBT+ e seus aliados. Nossa missão é fornecer suporte, educação e recursos para promover a igualdade, a diversidade e a acessibilidade.
-                             <br /> <br /><button>Saiba mais</button>
-                            </p>
-                                          
-                        </div>
-                        
-                    </div>
+function Home() {
+    return (
+        <>
+            <div className="relative z-20 flex bg-scroll bg-cover bg-center overflow-hidden h-screen bg-gray-50" style={{ backgroundImage: "url('./background/home-banner-1.png')" }}>
+                <div class="relative mt-8 hidden sm:block">
+                    <img src="name-rainbowcare-vector.svg" class="max-w-xs m-auto md:max-w-sm" />
+                    <p className="mt-2 mb-10 flex justify-center text-base text-center text-gray-900 font-semibold">
+                        A Plataforma de Acolhimento LGBT+ criada com o compromisso<br></br>
+                        inabalável de oferecer um espaço seguro, inclusivo e acolhedor para<br></br>
+                        todas as pessoas que fazem parte da comunidade e seus aliados.
+                    </p>
                 </div>
             </div>
-        </div>
+
+            <div className="relative z-20 flex bg-scroll bg-auto overflow-hidden h-screen bg-gray-50" style={{ backgroundImage: "url('./background/home-banner-2.png')" }}>
+            </div>
+            
+            <div className="relative z-20 flex bg-scroll bg-auto overflow-hidden h-screen bg-gray-50" style={{ backgroundImage: "url('./background/home-banner-4.png')" }}>
+            </div>
+        </>
     );
 }
 
